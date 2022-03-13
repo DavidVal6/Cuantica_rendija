10693328db336c143afba11a08907e2018d643bb
DavidVal6 <98176834+DavidVal6@users.noreply.github.com>
2022-03-13 16:26:36 -0500

    Verison 3

--- Doble_rendija.py
+++ Doble_rendija.py
@@ -4,30 +4,22 @@
  - ANDRES AJIACO
  CNYT , COMPETENCIA RENDIJA
  """
-import math
 from sys import stdin
+import math
 import numpy as np
 
-def accion(m1,estado0):
-    xn = np.dot(m1,estado0)
-    return(xn)
-
-
-def filas(m):
-    return (len(m))
-
-
-def columnas(m):
-    return len(m[0])
+def accion(estado,rendija):
+    """Este hallara la accione entre la matriz de adyacencia y el vector de estado de la rendija"""
+    return(np.dot(rendija,estado))
 
 
 def main():
-    m1 = [[0,0,0,0,0,0,0,0],[0.5,0,0,0,0,0,0,0],[0.5,0,0,0,0,0,0,0],[0,0.3,0,1,0,0,0,0],[0,0.3,0,0,1,0,0,0],[0,0.3,0.3,0,0,1,0,0],[0,0,0.3,0,0,0,1,0],[0,0,0.3,0,0,0,0,1]]
+    ticks = stdin.readline().strip()
+    ticks = int(ticks)
+    rendija = [[0,0,0,0,0,0,0,0],[1/math.sqrt(2),0,0,0,0,0,0,0],[1/math.sqrt(2),0,0,0,0,0,0,0], [0,-1/math.sqrt(6)+1j/math.sqrt(6),0,1,0,0,0,0],[0,-1/math.sqrt(6)-1j/math.sqrt(6),0,0,1,0,0,0],[0,1/math.sqrt(6)-1j/math.sqrt(6),-1/math.sqrt(6)+1j/math.sqrt(6),0,0,1,0,0],[0,0,-1/math.sqrt(6)-1j/math.sqrt(6),0,0,0,1,0],[0,0,1/math.sqrt(6)-1j/math.sqrt(6),0,0,0,0,1]]
     estado = [1,0,0,0,0,0,0,0]
-    ticks_totales = stdin.readline().strip()
-    ticks_totales = int(ticks_totales)
-
-    for i in range(ticks_totales):
-        estado = accion(m1, estado)
+    for i in range(ticks):
+        estado = accion(estado, rendija)
     print(estado)
+
 main()
